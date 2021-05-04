070a9e25776a4427d9daf8e5384a0451d2f15a26
SrJandro <82890203+SrJandro@users.noreply.github.com>
2021-05-04 16:26:22 +0200

    segon

--- supermercat/src/Main.java
+++ supermercat/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
+//* Gestió de vendes ...*/
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola món");
+        saludaMain();
+        Segon.saluda();
     }
+
+
+
+public static void saludaMain(){
+    System.out.println("Hola desde Main!");
+}
+
+
+void menuPrincipal(){
+    System.out.println("Gestió de supermercat");
+    System.out.println("1. Gestió Estoc");
+    System.out.println("2. Gestió Vendes");
+    System.out.println("3. Sortir");
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("\nIntrodueix la opció (1-3): ");
+
+    //TODO llegir entrada
+    //TODO cridar opció escollida
+}
+
+
+static void leerEstoc(){
+
+}
 }
